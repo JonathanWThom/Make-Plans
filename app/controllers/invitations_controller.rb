c4717da9471e652af3deefbaea6849b7faadbf2c
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-06-18 21:34:49 -0700

    Can rsvp as existing user

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,7 +7,8 @@ class InvitationsController < ApplicationController
   def rsvp
     invitation = Invitation.find_by_uuid(params[:uuid])
     invitation.users << current_user
+    invitation.pending_invitations.where(user_email: current_user.email).first.destroy
     flash[:notice] = "Congrats, you've made plans!"
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 end
