190d7906b49f397220a20393ee92cc25a5baaa3c
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-09-04 17:11:26 -0700

    Refactoring of hidden fields because of bootstrap's helpfulness

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -4,12 +4,12 @@ $.extend(CARDS, {
   toggleCardInfo: {
     init: function() {
       $("body").on("click", ".fa-caret-up", function() {
-        $(this).parents(".card").find(".hidden").show();
+        $(this).parents(".card").find(".display-none").show();
         $(this).hide();
       });
 
       $("body").on("click", ".fa-caret-down", function() {
-        $(this).parents(".card").find(".hidden").hide();
+        $(this).parents(".card").find(".display-none").hide();
         $(this).parents(".card").find(".fa-caret-up").show();
       })
     }
