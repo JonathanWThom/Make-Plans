2076ddbbf3657581c437f3b081e256e498551f74
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-10-20 22:37:03 -0700

    Require location

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,7 +7,7 @@ class Activity < ActiveRecord::Base
   has_many :events, through: :invitations, dependent: :destroy
   after_commit :convert_happening_at, on: [:create, :update]
 
-  validates :title, presence: true
+  validates :title, :location, presence: true
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates_with DateValidator
